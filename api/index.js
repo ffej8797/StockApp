2e48040bdb78b5b063ec0ce7e2e759b7652a37da
Jeff <cchsiao.jeff@gmail.com>
2026-07-10 16:31:48 +0800

    [feature]express加上middleware for log

--- api/index.js
+++ api/index.js
@@ -4,11 +4,18 @@ import lineWebhook from './app/lineWebhook.js';
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Method:', req.method);
+  console.log('URL:', req.url);
+  console.log('Body:', JSON.stringify(req.body, null, 2));
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/lineWebhook', (req, res) => { lineWebhook(req, res) });
+app.post('/lineWebhook', async(req, res) => { await lineWebhook(req, res) });
 
 app.get('/test', (req, res) => {
   const env = process.env.TEST;
