4f10926afb14c128851b0b37f06c0d80ee0cb84f
Jeff <cchsiao.jeff@gmail.com>
2026-07-10 14:39:36 +0800

    [update]express solve body

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import lineWebhook from './app/lineWebhook.js';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
